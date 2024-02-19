5dec4bfa3d424ac3dcff0b7ff913147fee045734
Brandon DeRosier <x@bdero.me>
2024-02-18 19:19:27 -0800

    Lazy load the base shader bundle on first use.

--- lib/shaders.dart
+++ lib/shaders.dart
@@ -1,4 +1,18 @@
 import 'package:flutter_gpu/gpu.dart' as gpu;
 
-final gpu.ShaderLibrary baseShaderLibrary = gpu.ShaderLibrary.fromAsset(
-    'packages/flutter_scene/assets/base.shaderbundle')!;
+final String _kBaseShaderBundlePath =
+    'packages/flutter_scene/assets/base.shaderbundle';
+
+gpu.ShaderLibrary? _baseShaderLibrary = null;
+gpu.ShaderLibrary get baseShaderLibrary {
+  if (_baseShaderLibrary != null) {
+    return _baseShaderLibrary!;
+  }
+  _baseShaderLibrary = gpu.ShaderLibrary.fromAsset(_kBaseShaderBundlePath);
+  if (_baseShaderLibrary != null) {
+    return _baseShaderLibrary!;
+  }
+
+  throw Exception(
+      "Failed to load base shader bundle! ($_kBaseShaderBundlePath)");
+}
