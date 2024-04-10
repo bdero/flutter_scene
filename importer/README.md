9d5af58355d735e444f098058e5c96224b44e7e6
Brandon DeRosier <bdero@google.com>
2024-04-10 08:48:24 -0700

    Generate importer runtime package

---
+++ importer/README.md
@@ -0,0 +1,7 @@
+A 3D model importer for Flutter Scene.
+
+## Features
+
+This packages provides an offline 3D model importer for the `flutter_scene` package, consisting of:
+* An offline importer binary that converts GLB files (the glTF binary format) into `model` files.
+* A Dart runtime that contains tools for deserializing the imported `model` files.
--- importer/build.sh
+++ importer/build.sh
@@ -4,6 +4,21 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 cd $SCRIPT_DIR
 
+source ../build_utils.sh
+
+echo "Building flatbuffer Dart runtime..."
+FLATC_EXE="$(GetFlatcExecutable)"
+if [ ! -f "$FLATC_EXE" ]; then
+    echo "FlatC not found. Can't build the flatbuffer Dart runtime!"
+    exit 1
+fi
+$FLATC_EXE \
+  -o lib/generated \
+  --warnings-as-errors \
+  --gen-object-api \
+  --filename-suffix _flatbuffers \
+  --dart scene.fbs
+
 echo "Building importer..."
 
 mkdir -p build
