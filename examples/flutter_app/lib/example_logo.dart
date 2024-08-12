f8b9f7f8b2631a9b6d076ff69093a0b39ae640b8
Brett Morgan <brettmorgan@google.com>
2024-08-12 15:54:10 +1000

    Make `flutter_app` work on flutter `master` channel

--- examples/flutter_app/lib/example_logo.dart
+++ examples/flutter_app/lib/example_logo.dart
@@ -3,7 +3,6 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_scene/camera.dart';
-import 'package:flutter_scene/material/environment.dart';
 import 'package:flutter_scene/node.dart';
 import 'package:flutter_scene/scene.dart';
 
@@ -24,7 +23,7 @@ class ExampleLogoState extends State<ExampleLogo> {
   @override
   void initState() {
     final loadModel =
-        Node.fromAsset('build/models/DamagedHelmet.model').then((value) {
+        Node.fromAsset('build/models/flutter_logo_baked.model').then((value) {
       value.name = 'FlutterLogo';
       scene.add(value);
       debugPrint('Model loaded: ${value.name}');
