52f2c100d5c7fff444c994651ee3df2a19412b29
Brandon DeRosier <x@bdero.me>
2026-08-01 23:56:31 -0700

    Skip code-only shader hook runs

--- packages/flutter_scene/hook/build.dart
+++ packages/flutter_scene/hook/build.dart
@@ -1,9 +1,13 @@
+import 'package:data_assets/data_assets.dart';
 import 'package:hooks/hooks.dart';
 
 import 'package:flutter_gpu_shaders/build.dart';
 
 void main(List<String> args) async {
   await build(args, (config, output) async {
+    if (!config.config.buildDataAssets) {
+      return;
+    }
     await buildShaderBundleJson(
       buildInput: config,
       buildOutput: output,
